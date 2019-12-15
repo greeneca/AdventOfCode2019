db495f2db4bc3937af27ab121f6401186e645d2d
Charles Greene <charles.greene@redspace.com>
2019-12-15 09:34:12 -0400

    Add day 14

---
+++ lib/day14-2.rb
@@ -0,0 +1,82 @@
+module P142
+  class P142
+    def run(input)
+      @reactions = {}
+      input.each do |reaction|
+        inputs, output = reaction.strip.split("=>")
+        inputs  = inputs.strip.split(",").map{|ingredient| process_ingredient(ingredient)}
+        output = process_ingredient(output)
+        @reactions[output[1]] = Reaction.new(output, inputs)
+      end
+      total_ore = 1000000000000
+      ore_requirement = 0
+      fuel = total_ore/get_ore_requirement(1)
+      [1000, 100, 10, 1].each do |increment|
+        while (ore_requirement < total_ore)
+          fuel += increment
+          ore_requirement = get_ore_requirement(fuel)
+        end
+        fuel -= increment
+        ore_requirement = 0
+      end
+      #Off by one error on final input for some reason but not on sample input
+      puts "Fuel: #{fuel-1}"
+    end
+
+    def get_ore_requirement(fuel)
+      requirements = [[fuel,  :FUEL]]
+      while not done(requirements)
+        requirements = requirements.inject([]) do |new_requirements,requirement|
+          if @reactions[requirement[1]]
+            new_requirements += @reactions[requirement[1]].get_requirements(requirement[0])
+          else
+            new_requirements.push(requirement)
+          end
+        end
+        requirements = combine(requirements)
+      end
+      requirements.first.first
+    end
+
+    def done(requirements)
+      requirements.count == 1 and requirements[0][1] == :ORE
+    end
+
+    def process_ingredient(ingredient_string)
+      ingredient = ingredient_string.strip.split(" ")
+      ingredient[0] = ingredient[0].to_i
+      ingredient[1] = ingredient[1].to_sym
+      ingredient
+    end
+
+    def combine(requirements)
+      new_requirements = Marshal.load(Marshal.dump(requirements.uniq{|req| req.last}))
+      new_requirements.map! do |requirement|
+        matching_requirements = requirements.select{|req| req[1] == requirement[1]}
+        requirement[0] = matching_requirements.inject(0) {|sum,req| sum += req[0]}
+        requirement
+      end
+      new_requirements
+    end
+
+  end
+
+  class Reaction
+    def initialize(creates, inputs)
+      @creates = creates[1]
+      @create_count = creates[0]
+      @inputs = inputs
+      @extra = 0
+    end
+    def get_requirements(count)
+      count -= @extra
+      reaction_count = (count.to_f/@create_count).ceil
+      @extra = (@create_count*reaction_count) - count
+      requirements = Marshal.load(Marshal.dump(@inputs))
+      requirements.map do |input|
+        input[0] *= reaction_count
+        input
+      end
+    end
+  end
+end
